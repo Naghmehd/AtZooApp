8a3e62161686f35640b0da427317d9c0232ece22
Naghmeh Shirazi <naghmehd@gmail.com>
2017-03-02 18:20:57 -0600

    Added animal_type and cage_type to the respective models

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -7,4 +7,28 @@ class Animal < ApplicationRecord
   def herbivore?
     'Herbivore' == self.type
   end
+
+  def unknownvore?
+    'Unknownvore' == self.type
+  end
+
+  def omnivore?
+    'Omnivore' == self.type
+  end
+
+  def is_carnivore?
+    carnivore == true && herbivore == false
+  end
+
+  def is_herbivore?
+    herbivore == true && carnivore == false
+  end
+
+  def is_unknownvore?
+    herbivore == false && carnivore == false
+  end
+
+  def is_omnivore?
+    herbivore == true && carnivore == true
+  end
 end
--- app/models/cage.rb
+++ app/models/cage.rb
@@ -1,11 +1,31 @@
 class Cage < ApplicationRecord
   belongs_to :zoo
 
+  def add_to_correct_cage(animal)
+    if animal.is_carnivore?
+      carnicage << animal
+    elsif animal.is_herbivore?
+      herbicage << animal
+    elsif animal.is_unknownvore
+      holdcage << animal
+    else
+      omnicage << animal
+    end
+  end
+
   def carnicage?
     'Carnicage' == self.type
   end
 
   def herbicage?
     'Herbicage' == self.type
+  end
+
+  def holdcage?
+    'Holdcage' == self.type
+  end
+
+  def omnicage?
+    'Omnivore' == self.type
   end
 end
